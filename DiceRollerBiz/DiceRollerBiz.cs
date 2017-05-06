60a62743158fb04706ae74bdc9ed47f3a224bc74
JasonAndersen7 <andersen.jason7@gmail.com>
2017-05-06 16:09:25 -0600

    Add files via upload

---
+++ DiceRollerBiz/DiceRollerBiz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiceRollerData;
+using DiceRollerModels;
+namespace DiceRollerBiz
+{
+    public class DiceRollerBiz : IDiceRollerBiz
+    {
+        public Die RollTwoDice(int max)
+        {
+
+            IDiceRollerData diceRoller = new DiceRollerData.DiceRollerData();
+
+            Die result = diceRoller.RollTwoDice(max);
+
+            return result;
+
+        }
+
+        public Die RollDice(Die d)
+        {
+            IDiceRollerData diceRoller = new DiceRollerData.DiceRollerData();
+            var result = diceRoller.RollDice(d);
+            return result;
+
+        }
+
+    }
+}
