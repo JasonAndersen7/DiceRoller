065a1917e13c86a6a0ea8f3ac4f99a43ab8d01bc
JasonAndersen7 <andersen.jason7@gmail.com>
2017-05-07 18:33:17 -0600

    Add files via upload

--- DiceRollerData/IDiceRollerData.cs
+++ DiceRollerData/IDiceRollerData.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DiceRollerModels;
-namespace DiceRollerData
+namespace DiceRollerDataComponent
 {
     public interface IDiceRollerData
     {
-
+        /// <summary>
+        /// default method to roll two dice
+        /// </summary>
+        /// <param name="max">the number of sides of the dice</param>
+        /// <returns></returns>
         Die RollTwoDice(int max);
 
+
+        /// <summary>
+        /// Future functionality to roll any number of dice
+        /// </summary>
+        /// <param name="d">the list of dice to roll </param>
+        /// <returns></returns>
         Die RollDice(Die d);
 
     }
